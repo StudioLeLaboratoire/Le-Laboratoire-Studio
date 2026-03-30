cfed3cb2422e7dbc3e974b1aebe74dcf8c3a4ac5
StudioLeLaboratoire <contact.studiolabo@gmail.com>
2026-03-30 22:28:09 +0200

    Update getSlots.js

--- netlify/functions/getSlots.js
+++ netlify/functions/getSlots.js
@@ -2,18 +2,17 @@ const https = require('https');
 
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwQIoIzjSY6adzgojN4GbFTuHkJvHdCqvAjt-ayRHOoziZZddvp6OH1UwfrEXkAAxUgNQ/exec?action=getSlots';
 
-function getURL(url) {
+function getURL(url, redirectCount = 0) {
   return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
+    if (redirectCount > 10) return reject(new Error('Too many redirects'));
+    const lib = url.startsWith('https') ? https : require('http');
+    lib.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' } }, (res) => {
+      if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
+        return getURL(res.headers.location, redirectCount + 1).then(resolve).catch(reject);
+      }
       let data = '';
       res.on('data', chunk => data += chunk);
-      res.on('end', () => {
-        if (res.statusCode === 302 && res.headers.location) {
-          getURL(res.headers.location).then(resolve).catch(reject);
-        } else {
-          resolve(data);
-        }
-      });
+      res.on('end', () => resolve(data));
     }).on('error', reject);
   });
 }
@@ -21,13 +20,14 @@ function getURL(url) {
 exports.handler = async function(event, context) {
   try {
     const data = await getURL(APPS_SCRIPT_URL);
+    const parsed = JSON.parse(data);
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
       },
-      body: data
+      body: JSON.stringify(parsed)
     };
   } catch (error) {
     return {
